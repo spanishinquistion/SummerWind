36e126779a2b03c4792fb4cc37fe172935caa989
WindMark <44115005+spanishinquistion@users.noreply.github.com>
2020-07-05 18:54:25 +0200

    Add files via upload

---
+++ webfiles/navbar.js
@@ -0,0 +1,8 @@
+var motto = "Making Data Information";
+var navbar_dd = {}
+navbar_dd.tab1 = "Overview";
+navbar_dd.tab2 = "Data";
+navbar_dd.tab3 = "Methods and Information";
+navbar_dd.baseline = "<a class=\"dropdown-item\" href=\"../../meas_data/00003/dashboard_00003.html\">Preliminary Dashboard</a>";
+navbar_dd.hi_res = "<a class=\"dropdown-item\" href=\"../../meas_data/00399/hi_res_dashboard_00399.html\">High Resolution Dashboard</a>";
+navbar_dd.try = "<a class=\"dropdown-item\" href=\"../../try_data/434_63/try_dashboard_434_63.html\">DWD TRY Dashboard</a>";
